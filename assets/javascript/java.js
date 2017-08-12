5086366572bd21ebe2d279ac5af3968ca80ae5c2
DESKTOP-U6KB5LI\march <marchettipm@gmail.com>
2017-08-12 11:09:44 -0400

    fixed

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -18,7 +18,25 @@ $("#modalDiv1").on("click", function() {
     $("#modalDiv1").css("display", "none")
 });
 
+var modal2 = document.getElementById('myModal2');
 
+var btn2 = document.getElementById("myBtn2");
+
+var span2 = document.getElementsByClassName("close")[0];
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+    $("#modalDiv1").css("display", "block")
+}
+
+span2.onclick = function() {
+    modal2.style.display = "none";
+}
+
+$("#modalDiv1").on("click", function() {
+    modal2.style.display = "none";
+    $("#modalDiv1").css("display", "none")
+});
 
 
 
