9dcab539e3518b104e196aa6da2a57f0423822c5
Philippe Dixon <pdixon555@hotmail.com>
2017-08-13 19:17:13 -0400

    added signin to main page

--- assets/javascript/apiskeleton.js
+++ assets/javascript/apiskeleton.js
@@ -13,133 +13,144 @@ var config = {
 /// Phil's vars and function declarations end
 
 $(document).ready(function() {
-firebase.initializeApp(config);
-  
-	$("#submit").on("click", function(event) {
-		event.preventDefault();
-		var query = $("#search").val();
-		var queryUrl2 = "https://api.spotify.com/v1/search?query=" + query + "&type=artist&limit=5";
-		var spotifyToken = "BQA5Px9_qwjusHi3-EHIFoZ_muJ5u0Lls31PxmHfeEl0OPDy0e-Dk53HgKTxrqf1g-HlSk4fBynhZdmWaQGFs49PHTn4gg3SnklWAUFrvLJjijSif1WCyMRLHxzc04ySZzHz_vMNK7mC"
-	//Begin of ticketmaster api call
+    firebase.initializeApp(config);
 
-	//Begin last.fm api call
-	//Keys for last.fm api 
-	// API key	7e69c5904858f852ede1f60d3d4788bf
-	// Shared secret	7a25cc6363b28ebadc49a4de8bb8c1b7
+    $("#submit").on("click", function(event) {
+        event.preventDefault();
+        var query = $("#search").val();
+        var queryUrl2 = "https://api.spotify.com/v1/search?query=" + query + "&type=artist&limit=5";
+        var spotifyToken = "BQA5Px9_qwjusHi3-EHIFoZ_muJ5u0Lls31PxmHfeEl0OPDy0e-Dk53HgKTxrqf1g-HlSk4fBynhZdmWaQGFs49PHTn4gg3SnklWAUFrvLJjijSif1WCyMRLHxzc04ySZzHz_vMNK7mC"
+            //Begin of ticketmaster api call
 
-	//Begin spotify api call
-		// Spotify Client Credentials Flow
-		// var clientId = "977af0fc81734cad8b087e49d2597c42";
-		// var clientSecret = "c6d73d59372241198af8835721ef0f97";
+        //Begin last.fm api call
+        //Keys for last.fm api 
+        // API key	7e69c5904858f852ede1f60d3d4788bf
+        // Shared secret	7a25cc6363b28ebadc49a4de8bb8c1b7
 
-		// $.ajax({
-		// 	method: "POST",
-		// 	url: "https://accounts.spotify.com/api/token",
-		// 	grant_type: "client_credentials"
-		// 	headers: {
-		// 		Authorization: "Basic " + clientId + ":" + clientSecret 
-		// 	},
-		// }).done( function(response) {
-		// 	console.log(response);
-		// })
-		$.ajax({
-     		url: queryUrl2,
-     		headers: {
-      		  Authorization: 'Bearer ' + spotifyToken
-      		},
-   		})
-   		.done( function(response) {
-		 	 console.log(response.artists.items);
-		 	 $("#results").empty();
-		 	 for (var i = 0; i < 5; i++) {
-		 	 	$("#results").append("<button id='artistbutton' class='" + response.artists.items[i].id + "' type='submit'>" + response.artists.items[i].name + "</button>")
-		 	 }
-		})
-	})	
+        //Begin spotify api call
+        // Spotify Client Credentials Flow
+        // var clientId = "977af0fc81734cad8b087e49d2597c42";
+        // var clientSecret = "c6d73d59372241198af8835721ef0f97";
 
-	function runTm() {
-		var artistName = $(this).text();
-		var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artistName + "&apikey=STp440AwxFJGrUI9c9fFXpXQ8dZZuGow"
-		var token = "BQDjYhg68AGCrPp6AhyVllcGHXwmo8GX3YZSuaEAd4lqO6fIMoeONqk9DcBfYIRxoMiCp1P0wK4Mzj_ej_GZ0W7LehVmIbuG45jN9Y-reFnh13H78HpuBF1u3s0ILI8gE7PbBgmmb5rv"	
-		$.ajax({
-		  method:"GET",
-		  url: queryUrl,
-		  async:true,
-		  dataType: "json",
-		  success: function(json) {
-		              console.log(json);
-		              // Parse the response.
-		              // Do other things.
-		           },
-		  error: function(xhr, status, err) {
-		              // This time, we do not end up here!
-		           }
-		}).done(function(response){
-			console.log(response._embedded);
-			for (var i = 0; i < 7; i++) {
-				var date = response._embedded.events[i].dates.start.localDate;
-				var location = response._embedded.events[i]._embedded.venues[0].city.name;
-				var venue = response._embedded.events[i]._embedded.venues[0].name;
-				var tickets = response._embedded.events[i].url;
-				$("#tm").append("<tr><td>" + date + "</td><td>" + location + "</td><td>" + venue + "</td><td>" + "<a href='" +  tickets + "'>TICKETS</a></td></tr>");
-			}
-		});	
-	}	
-	
-	$(document).on("click", "#artistbutton", runTm)
+        // $.ajax({
+        // 	method: "POST",
+        // 	url: "https://accounts.spotify.com/api/token",
+        // 	grant_type: "client_credentials"
+        // 	headers: {
+        // 		Authorization: "Basic " + clientId + ":" + clientSecret 
+        // 	},
+        // }).done( function(response) {
+        // 	console.log(response);
+        // })
+        $.ajax({
+                url: queryUrl2,
+                headers: {
+                    Authorization: 'Bearer ' + spotifyToken
+                },
+            })
+            .done(function(response) {
+                console.log(response.artists.items);
+                $("#results").empty();
+                for (var i = 0; i < 5; i++) {
+                    $("#results").append("<button id='artistbutton' class='" + response.artists.items[i].id + "' type='submit'>" + response.artists.items[i].name + "</button>")
+                }
+            })
+    })
 
-	function displayAlbums() {
-		var artistId = $(this).attr("class");
-		console.log(artistId);
-		var queryId = "https://api.spotify.com/v1/artists/" + artistId + "/albums?offset=0&limit=6&album_type=album&market=ES"
-		$.ajax({
-			url: queryId,
-			headers: {
-				Authorization: 'Bearer  ' + spotifyToken
-			},
-		}).done( function(response) {
-			console.log(response);
-			$("#bandpic").attr("src", );
-			// for (var i = 0; i < 6; i++) {
-			// 	$("#container").append("<img id='" + response.items[i].uri + "'class='" + response.items[i].id + "'src='" + response.items[i].images[0].url + "'>")
-			// }
-		})
-	}
+    function runTm() {
+        var artistName = $(this).text();
+        var queryUrl = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" + artistName + "&apikey=STp440AwxFJGrUI9c9fFXpXQ8dZZuGow"
+        var token = "BQDjYhg68AGCrPp6AhyVllcGHXwmo8GX3YZSuaEAd4lqO6fIMoeONqk9DcBfYIRxoMiCp1P0wK4Mzj_ej_GZ0W7LehVmIbuG45jN9Y-reFnh13H78HpuBF1u3s0ILI8gE7PbBgmmb5rv"
+        $.ajax({
+            method: "GET",
+            url: queryUrl,
+            async: true,
+            dataType: "json",
+            success: function(json) {
+                console.log(json);
+                // Parse the response.
+                // Do other things.
+            },
+            error: function(xhr, status, err) {
+                // This time, we do not end up here!
+            }
+        }).done(function(response) {
+            console.log(response._embedded);
+            for (var i = 0; i < 7; i++) {
+                var date = response._embedded.events[i].dates.start.localDate;
+                var location = response._embedded.events[i]._embedded.venues[0].city.name;
+                var venue = response._embedded.events[i]._embedded.venues[0].name;
+                var tickets = response._embedded.events[i].url;
+                $("#tm").append("<tr><td>" + date + "</td><td>" + location + "</td><td>" + venue + "</td><td>" + "<a href='" + tickets + "'>TICKETS</a></td></tr>");
+            }
+        });
+    }
 
-	// function displayTracks() {
-	// 	var albumName = $(this).attr("class");
-	// 	var albumUri = $(this).attr("id");
-	// 	$("#container").empty();
-	// 	$("iframe").attr("src", "https://open.spotify.com/embed?uri=" + albumUri + "")
-	// 	console.log(albumUri);
-	// 	console.log(albumName);
-	// 	var queryAlbumId = "https://api.spotify.com/v1/albums/" + albumName + "/tracks";
-	// 	$.ajax({
-	// 		url: queryAlbumId,	
-	// 		headers: {
-	// 			Authorization: 'Bearer ' + spotifyToken
-	// 		},
-	// 	}).done( function(response) {
-	// 		console.log(response);
-	// 		$("#container").empty();
-	// 		for (var i = 0; i < response.items.length; i++) {
-	// 			$("#container").append("<p class='" + [i] + "'>" + response.items[i].track_number + "</p>");
-	// 			$("." + [i] + "").append("<p>" + response.items[i].name + "</p>");
-	// 		}
-	// 	})
-	// }
+    $(document).on("click", "#artistbutton", runTm)
 
-	// $(document).on("click", "img", displayTracks)
+    function displayAlbums() {
+        var artistId = $(this).attr("class");
+        console.log(artistId);
+        var queryId = "https://api.spotify.com/v1/artists/" + artistId + "/albums?offset=0&limit=6&album_type=album&market=ES"
+        $.ajax({
+            url: queryId,
+            headers: {
+                Authorization: 'Bearer  ' + spotifyToken
+            },
+        }).done(function(response) {
+            console.log(response);
+            $("#bandpic").attr("src", );
+            // for (var i = 0; i < 6; i++) {
+            // 	$("#container").append("<img id='" + response.items[i].uri + "'class='" + response.items[i].id + "'src='" + response.items[i].images[0].url + "'>")
+            // }
+        })
+    }
 
-	//Phils code start
+    // function displayTracks() {
+    // 	var albumName = $(this).attr("class");
+    // 	var albumUri = $(this).attr("id");
+    // 	$("#container").empty();
+    // 	$("iframe").attr("src", "https://open.spotify.com/embed?uri=" + albumUri + "")
+    // 	console.log(albumUri);
+    // 	console.log(albumName);
+    // 	var queryAlbumId = "https://api.spotify.com/v1/albums/" + albumName + "/tracks";
+    // 	$.ajax({
+    // 		url: queryAlbumId,	
+    // 		headers: {
+    // 			Authorization: 'Bearer ' + spotifyToken
+    // 		},
+    // 	}).done( function(response) {
+    // 		console.log(response);
+    // 		$("#container").empty();
+    // 		for (var i = 0; i < response.items.length; i++) {
+    // 			$("#container").append("<p class='" + [i] + "'>" + response.items[i].track_number + "</p>");
+    // 			$("." + [i] + "").append("<p>" + response.items[i].name + "</p>");
+    // 		}
+    // 	})
+    // }
 
-     // add firebase
+    // $(document).on("click", "img", displayTracks)
+
+    //Phils code start
+    var user = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log(user);
+            console.log('signed in');
+        } else {
+            if (window.location.href ===
+                "file:///C:/Users/Philippe/Dropbox/Desktop/unc/band-project/layout333.html") {
+                window.location.href = "index.html";
+            }
+        }
+    });
+
     $('#create').on('click', function() {
         if ($('#createPassword').val().trim() === $('#confirmPassword').val().trim()) {
             console.log('matched');
             email = $('#createEmail').val().trim();
             password = $('#createPassword').val().trim();
-  
+
             firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
                 // Handle Errors here.
                 var errorCode = error.code;
@@ -163,26 +174,28 @@ firebase.initializeApp(config);
     $('#signIn').on('click', function() {
         email = $('#email').val().trim();
         password = $('#password').val().trim();
-        console.log( $('#email').val().trim() );
-        console.log( $('#password').val().trim());
-        // 	console.log(email);
-        // 	console.log(password);
-        // 	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // // Handle Errors here.
-        // var errorCode = error.code;
-        // var errorMessage = error.message;
-        // // ...
-        // }).then(function() {
-        // 	window.location.href = "slick.html";
-        // 	console.log(firebase.auth().currentUser.uid);
-        // });
+        console.log($('#email').val().trim());
+        console.log($('#password').val().trim());
+        console.log(email);
+        console.log(password);
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+        }).then(function() {
+            window.location.href = "layout333.html";
+            console.log(firebase.auth().currentUser.uid);
+        });
     });
 
-    // $('#signOut').on('click', function() {
-    // 	firebase.auth().signOut().then(function() {
-    // 	console.log('sign out successful')
-    // }).catch(function(error) {
-    // // An error happened.
-    // });
-    // });
+    $('#logOut').on('click', function(e) {
+        e.preventDefault();
+        firebase.auth().signOut().then(function() {
+            console.log('sign out successful');
+            window.location.href = "index.html";
+        }).catch(function(error) {
+            // An error happened.
+        })
+    });
 });
