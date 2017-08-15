bcc059c6278357eeb156d1376af067cd68c33080
Philippe Dixon <pdixon555@hotmail.com>
2017-08-15 19:47:32 -0400

    remove hardcoded user sign in

--- assets/javascript/apiskeleton.js
+++ assets/javascript/apiskeleton.js
@@ -254,8 +254,8 @@ $(document).ready(function() {
                     favorites: "",
                     currentArtist: ""
                 });
-                // window.location.href = "layout333.html";
-                window.location.href = "https://psd314.github.io/band-project/layout333.html";
+                window.location.href = "layout333.html";
+                // "https://psd314.github.io/band-project/layout333.html"
             }).catch(function(error) {
                 // Handle Errors here.
                 var errorCode = error.code;
@@ -273,8 +273,8 @@ $(document).ready(function() {
         console.log(email);
         console.log(password);
         firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-            // window.location.href = "layout333.html";
-            window.location.href = "https://psd314.github.io/band-project/layout333.html";
+            window.location.href = "layout333.html";
+            // "https://psd314.github.io/band-project/layout333.html"
         }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
@@ -295,8 +295,7 @@ $(document).ready(function() {
         e.preventDefault();
         firebase.auth().signOut().then(function() {
             console.log('sign out successful');
-            // window.location.href = "index.html";
-            window.location.href = "https://psd314.github.io/band-project/index.html";
+            window.location.href = "index.html";
         }).catch(function(error) {
             // An error happened.
         })
@@ -355,12 +354,12 @@ $(document).ready(function() {
 
         } else {
             console.log('not signed in');
-            // if (window.location.href === "file:///C:/Users/Philippe/Dropbox/Desktop/unc/band-project/layout333.html") {
-            //     window.location.href = "file:///C:/Users/Philippe/Dropbox/Desktop/unc/band-project/index.html";
-            // }
-            if (window.location.href === "https://psd314.github.io/band-project/layout333.html") {
-                window.location.href = "https://psd314.github.io/band-project/index.html";
+            if (window.location.href === "file:///C:/Users/Philippe/Dropbox/Desktop/unc/band-project/layout333.html") {
+                window.location.href = "file:///C:/Users/Philippe/Dropbox/Desktop/unc/band-project/index.html";
             }
+            // if (window.location.href === "https://psd314.github.io/band-project/layout333.html") {
+            //     window.location.href = "https://psd314.github.io/band-project/index.html";
+            // }
         }
     });
 
